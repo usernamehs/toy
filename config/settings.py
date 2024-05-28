3333322462a032d4db1528536ac2424dc36afc93
usernamehs <hsforwork123@gmail.com>
2024-05-28 15:29:50 +0900

    Feat: settings.py 복귀

--- config/settings.py
+++ config/settings.py
@@ -84,8 +84,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
+    # "http://127.0.0.1:5500",
+    # "http://localhost:5500",
 ]
 
 CORS_ORIGIN_WHITELIST = (
