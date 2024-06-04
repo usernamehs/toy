19506417d3a1e97377907ccaea8414d3deeacb4f
usernamehs <hsforwork123@gmail.com>
2024-06-04 17:49:32 +0900

    Feat: 추가된 링크의 수정

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "liongb.kro.kr",
+    "http://liongb.kro.kr",
     "http://liongb.kro.kr",
     "https://liongb.kro.kr/dist/",
     "http://liongb.kro.kr/dist",    
