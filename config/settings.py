57af70da2aa7d4a3bd68f8cfe4fda524e00b8b2b
usernamehs <hsforwork123@gmail.com>
2024-06-04 23:46:47 +0900

    Feat: 주석처리

--- config/settings.py
+++ config/settings.py
@@ -87,10 +87,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://liongb.kro.kr",
-    "http://liongb.kro.kr/dist/",
-    "https://liongb.kro.kr/dist/",
-    "liongb.kro.kr/dist",
-    "liongb.kro.kr",
+    # "http://liongb.kro.kr/dist/",
+    # "https://liongb.kro.kr/dist/",
+    # "liongb.kro.kr/dist",
+    # "liongb.kro.kr",
 ]
 
 # CORS_ORIGIN_WHITELIST = (
